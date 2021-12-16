1847e13deb031073af80a1c93556e44f4d38012b
ycjude <ycjude@hotmail.com>
2021-12-16 09:00:33 +0800

    更新主题样式

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,6 +1,3 @@
-function formatTime (date) {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDay()
-  return `${ year }-${ month }-${ day }`
+function formatTime (dateString) {
+  return dateString.split(' ')[0]
 }
