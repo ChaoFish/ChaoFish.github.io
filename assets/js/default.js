405c97b8c49319d78b599f15dc999b71fc8b5d1c
ycjude <ycjude@hotmail.com>
2021-12-16 09:12:06 +0800

    修改主题样式

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,3 +1,9 @@
 function formatTime (dateString) {
   return dateString.split(' ')[0]
 }
+
+function getAge () {
+  const current = new Date()
+  const currentYear = current.getFullYear()
+  return Number(currentYear) - 1993
+}
